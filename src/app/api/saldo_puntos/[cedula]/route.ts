35da3b9b07aabd651bc541e7b3596c038754c650
lexrojas <legalcr.rojas@gmail.com>
2024-11-26 01:01:17 -0600

    nextresponse

--- src/app/api/saldo_puntos/[cedula]/route.ts
+++ src/app/api/saldo_puntos/[cedula]/route.ts
@@ -1,32 +1,45 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import prisma from "@/app/lib/prisma";
 import { saldopuntos } from "@prisma/client/sql";
 import { typeSaldopuntos } from "@/app/lib/modelos";
 
 // Manejar solicitudes GET
 export async function GET(
-  req: Request,
-  { params }: { params: { cedula: string } },
-) {
-  const { cedula } = params;
-
-  if (!cedula) {
-    return NextResponse.json(
-      { error: "Cedula no debe quedar en blanco" },
-      { status: 400 },
-    );
-  }
-
+  request: NextRequest,
+  { params }: { params: Promise<{ cedula: string }> },
+): Promise<NextResponse> {
   try {
-    const saldo: typeSaldopuntos[] = await prisma.$queryRawTyped(
-      saldopuntos(cedula),
-    );
-    return NextResponse.json(saldo, { status: 200 });
+    const { cedula } = await params;
+    // ...rest of the handler
+    if (!cedula) {
+      return NextResponse.json(
+        { error: "Cedula no debe quedar en blanco" },
+        { status: 400 },
+      );
+    }
+
+    try {
+      const saldo: typeSaldopuntos[] = await prisma.$queryRawTyped(
+        saldopuntos(cedula),
+      );
+      return NextResponse.json(saldo, { status: 200 });
+    } catch (error) {
+      console.error("Error al obtener usuarios:", error);
+      return NextResponse.json(
+        { error: "Error al obtener los usuarios" },
+        { status: 500 },
+      );
+    }
   } catch (error) {
-    console.error("Error al obtener usuarios:", error);
     return NextResponse.json(
-      { error: "Error al obtener los usuarios" },
+      { error: `Error al obtener los usuarios ${error}` },
       { status: 500 },
     );
   }
+
+  // export async function GET(
+  //   req: Request,
+  //   { params }: { params: { cedula: string } },
+  // ) {
+  //   const { cedula } = params;
 }
